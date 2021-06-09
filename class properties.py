eb88b98f5a3cf4021a0f215e773392bdd4613f3b
jod35 <jodestrevin@gmail.com>
2021-06-09 17:57:21 +0300

    initial commit

---
+++ class properties.py
@@ -0,0 +1,13 @@
+#class properties
+class Boy:
+    first_name=None
+    second_name=None
+    age=None
+
+print(Boy.first_name)
+print(Boy.age)
+print(Boy.second_name)
+
+Boy.title=None
+
+print(Boy.title)
---
+++ classes.py
@@ -0,0 +1,13 @@
+#object oriented programming
+
+class Boy:
+    pass
+
+
+boy=Boy()
+boy1=Boy()
+boy2=Boy()
+
+print(boy)
+print(boy1)
+print(boy2)
