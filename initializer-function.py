f190eec44a21788ee74327f099c03f48ca7c071f
jod35 <jodestrevin@gmail.com>
2021-06-10 19:59:03 +0300

    initializer funtion

---
+++ initializer-function.py
@@ -0,0 +1,23 @@
+#the initializer function
+class Employee:
+    def __init__(self,id,age,email,first_name,last_name):
+        self.id=id
+        self.age=age
+        self.email=email
+        self.first_name=first_name
+        self.last_name=last_name
+
+        print("Object Created")
+
+
+emp1=Employee(id=1,age=22,email="jonathan@company.com",
+    first_name="Jonathan",
+    last_name="Bacon"
+)
+
+emp2=Employee(id=1,age=22,email="jane@company.com",
+    first_name="Jane",
+    last_name="Doe"
+)
+
+print(emp1.email)
